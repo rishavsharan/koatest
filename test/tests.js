4d8fc3e3046c9e03d453ddd948b625a87f6bdcbc
Rishav Sharan <rishav.sharan@live.com>
2018-07-30 17:54:09 +0530

    3rd commit

--- test/tests.js
+++ test/tests.js
@@ -1,13 +1,16 @@
 var assert = require('assert');
 
 const range = (max) => Array.from({length: max}, (value, key) => key)
-coll = range(1000)
+const pass_percentage = (p) => Math.random() > (1- p/100)
+
+const coll = range(100)
+const pass = 80
 
 describe('Feature: Server can respond to requests', () => {
     describe('User gotos base url and receives server greeting', () => {
         coll.forEach((i) => {
             it('Server should say Hello!', () => {
-                assert.equal(i, true);
+                assert.ok(pass_percentage(pass));
             });
         });
     });
