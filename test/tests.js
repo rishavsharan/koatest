7df69db8c9eb907678a3e118a44fd601d175fffe
Rishav Sharan <rishav.sharan@live.com>
2018-07-31 12:27:03 +0530

    added path to circleci config

--- test/tests.js
+++ test/tests.js
@@ -1,17 +1,22 @@
 var assert = require('assert');
 
 const range = (max) => Array.from({length: max}, (value, key) => key)
+const rand_tag = (list) => list[Math.floor(Math.random()*list.length)]
 const pass_percentage = (p) => Math.random() > (1- p/100)
 
-const coll = range(100000)
+const num_suites = 10
+const num_cases = 100
+
+const annotations = ["Alpha", "Beta", "Gamma", "Delta", "Theta"]
 const pass = 80
 
-describe('Feature: Server can respond to requests', () => {
-    describe('User gotos base url and receives server greeting', () => {
-        coll.forEach((i) => {
-            it(`test case ${i}`, () => {
+range(num_suites).forEach((i) => {
+    describe(`Test Suite #${i} >> `, () => {
+        range(num_cases).forEach((j) => {
+            it(`Test Case #${j} @${rand_tag(annotations)}`, () => {
                 assert.ok(pass_percentage(pass));
             });
         });
     });
 });
+
