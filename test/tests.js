b683a7c7ffb9968355d833ab52c70ab8243a7fad
Rishav Sharan <rishav.sharan@live.com>
2018-08-02 17:18:47 +0530

    increased tests to 100k

--- test/tests.js
+++ test/tests.js
@@ -4,8 +4,8 @@ const range = (max) => Array.from({length: max}, (value, key) => key)
 const rand_tag = (list) => list[Math.floor(Math.random()*list.length)]
 const pass_percentage = (p) => Math.random() > (1- p/100)
 
-const num_suites = 10
-const num_cases = 100
+const num_suites = 100
+const num_cases = 1000
 
 const annotations = ["Alpha", "Beta", "Gamma", "Delta", "Theta"]
 const pass = 80
